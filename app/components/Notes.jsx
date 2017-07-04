bd190a6f62b5346a72df0a0268a7d6c579986191
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2017-07-04 23:51:01 +0700

    add "add note" logic

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import uuid from 'uuid';
 
-const notes = [
-    {
-        id: uuid.v4(),
-        task: 'Learn React'
-    },
-    {
-        id: uuid.v4(),
-        task: 'Do laundry'
-    }
-];
-export default () => (
+// render each note
+export default ({notes}) => (
     <ul>{notes.map(note =>
         <li key={note.id}>{note.task}</li>
     )}</ul>
