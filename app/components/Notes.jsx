af7a6cbdfac42b1f097396be79195bde7ea34de0
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2017-07-09 21:30:48 +0700

    format note jsx

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -9,8 +9,8 @@ export default ({ notes, onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {
         {notes.map(({ id, editing, task }) =>
             <li key={id}>
                 <Note onClick={onNoteClick.bind(null, id)}>
-                    <Editable editing={editing} value={task} onEdit={onEdit.bind(null, id)} /> <
-                        button onClick={onDelete.bind(null, id)}>x</button>
+                    <Editable editing={editing} value={task} onEdit={onEdit.bind(null, id)} /> 
+                    <button onClick={onDelete.bind(null, id)}>x</button>
                 </Note>
             </li>
         )}</ul>
