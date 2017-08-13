cfd7b68f3b3e34fe96af9fcdd405691f7232fdfb
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2017-08-13 14:21:19 +0700

    add logic to attatch and detatch note to each lane, still need to revise this logic

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import uuid from 'uuid';
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
+import LaneActions from '../actions/LaneActions';
 import Notes from './Notes';
 
 const Lane = ({
-lane, notes, NoteActions, ...props
+    lane, notes, LaneActions, NoteActions, ...props
 }) => {
     const editNote = (id, task) => {
         NoteActions.update({ id, task, editing: false });
@@ -17,10 +18,19 @@ lane, notes, NoteActions, ...props
             id: noteId,
             task: 'New task'
         });
+
+        LaneActions.attachToLane({
+            laneId: lane.id,
+            noteId
+        });
     };
     const deleteNote = (noteId, e) => {
         e.stopPropagation();
         NoteActions.delete(noteId);
+        LaneActions.detachFromLane({
+            laneId: lane.id,
+            noteId
+        });
     };
     const activateNoteEdit = id => {
         NoteActions.update({ id, editing: true });
@@ -34,16 +44,30 @@ lane, notes, NoteActions, ...props
                 <div className="lane-name">{lane.name}</div>
             </div>
             <Notes
-                notes={notes}
+                notes={selectNotesByIds(notes, lane.notes)}
                 onNoteClick={activateNoteEdit}
                 onEdit={editNote}
                 onDelete={deleteNote} />
         </div>);
 };
 
+function selectNotesByIds(allNotes, noteIds = []) {
+  // `reduce` is a powerful method that allows us to
+  // fold data. You can implement `filter` and `map`
+  // through it. Here we are using it to concatenate
+  // notes matching to the ids.
+  return noteIds.reduce((notes, id) =>
+    // Concatenate possible matching ids to the result
+    notes.concat(
+      allNotes.filter(note => note.id === id)
+    )
+, []);
+}
+
 export default connect(({ notes }) => ({
     notes
 }), {
-        NoteActions
+    NoteActions,
+    LaneActions
     }
 )(Lane)
