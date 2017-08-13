ca3b4cee91a0ec3c0e13a6641e664006a2845e30
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2017-08-13 18:03:34 +0700

    refactor lane.jsx

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -4,25 +4,13 @@ import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
 import LaneActions from '../actions/LaneActions';
 import Notes from './Notes';
+import LaneHeader from './LaneHeader';
 
 const Lane = ({
     lane, notes, LaneActions, NoteActions, ...props
 }) => {
     const editNote = (id, task) => {
         NoteActions.update({ id, task, editing: false });
-    };
-    const addNote = e => {
-        e.stopPropagation();
-        const noteId = uuid.v4();
-        NoteActions.create({
-            id: noteId,
-            task: 'New task'
-        });
-
-        LaneActions.attachToLane({
-            laneId: lane.id,
-            noteId
-        });
     };
     const deleteNote = (noteId, e) => {
         e.stopPropagation();
@@ -37,12 +25,7 @@ const Lane = ({
     };
     return (
         <div {...props}>
-            <div className="lane-header">
-                <div className="lane-add-note">
-                    <button onClick={addNote}>+</button>
-                </div>
-                <div className="lane-name">{lane.name}</div>
-            </div>
+            <LaneHeader lane={lane} />
             <Notes
                 notes={selectNotesByIds(notes, lane.notes)}
                 onNoteClick={activateNoteEdit}
