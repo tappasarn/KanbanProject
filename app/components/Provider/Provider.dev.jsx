2c493ed690f4a1743b14ac10a56f98a2a2f44f41
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2017-08-13 13:08:27 +0700

    add perf tracking and chrome debug

--- app/components/Provider/Provider.dev.jsx
+++ app/components/Provider/Provider.dev.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import AltContainer from 'alt-container'; 
 import alt from '../../libs/alt';
 import setup from './setup';
+import chromeDebug from 'alt-utils/lib/chromeDebug';
+
 setup(alt);
+
+chromeDebug(alt);
+
+React.Perf = require('react-addons-perf');
+
 export default ({ children }) =>
     //set alt lib as app context
     <AltContainer flux={alt}>
